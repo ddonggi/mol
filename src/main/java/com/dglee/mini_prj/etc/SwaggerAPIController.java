5686586dcd371df64285ee5ed44d08e0ddba0d25
dglee <ldg6153@gmail.com>
2022-11-30 00:52:20 +0900

    ADD|question titles api

--- src/main/java/com/dglee/mini_prj/etc/SwaggerAPIController.java
+++ src/main/java/com/dglee/mini_prj/etc/SwaggerAPIController.java
@@ -15,6 +15,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * PackageName : com.dglee.mini_prj.etc
  * FileName : SwaggerAPIController
@@ -44,13 +49,42 @@ public class SwaggerAPIController {
 //            @Parameter(name = "hashtag", description = "검색한 해시태그", example = "['#자장면', '#중국집']")
     })
     @ResponseBody
-    @GetMapping( "/question/list")
+    @GetMapping("/question/list")
     public Page<Question> getQuestions(
-            @RequestParam(value = "page",defaultValue = "0") int page
+            @RequestParam(value = "page", defaultValue = "0") int page
 //            @RequestParam(value = "city") String city,
 //            @RequestParam(value = "hashtag", required = false) @Nullable String hashtag
     ) {
         Page<Question> paging = questionService.getList(page);
+        List<Question> questionList = paging.stream().filter((h) -> Boolean.parseBoolean(h.getTitle())).collect(Collectors.toList());
+        System.out.println("questionList = " + questionList);
         return paging;
+    }
+
+    @Operation(summary = "get test", description = "게시판 제목들을 가져오는 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = Question.class),mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
+    @Parameters({
+            @Parameter(name = "page", description = "페이지 넘버", example = "0"),
+//            @Parameter(name = "city", description = "도", example = "고양시"),
+//            @Parameter(name = "hashtag", description = "검색한 해시태그", example = "['#자장면', '#중국집']")
+    })
+    @ResponseBody
+    @GetMapping("/question/title")
+    public List<String> getQuestionsList(
+            @RequestParam(value = "page", defaultValue = "0") int page
+//            @RequestParam(value = "city") String city,
+//            @RequestParam(value = "hashtag", required = false) @Nullable String hashtag
+    ) {
+        Iterator<Question> iterator = questionService.getList(page).stream().iterator();
+        List<String> questionList = new ArrayList<>();
+        while(iterator.hasNext()) {
+            questionList.add(iterator.next().getTitle());
+        }
+        return questionList;
     }
 }
