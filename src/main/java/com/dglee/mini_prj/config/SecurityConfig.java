8f5c40afa55ae027acbd1e36498be04076791eb5
dglee <ldg6153@gmail.com>
2023-02-08 22:02:17 +0900

    ADD|Security Config

---
+++ src/main/java/com/dglee/mini_prj/config/SecurityConfig.java
@@ -0,0 +1,94 @@
+package com.dglee.mini_prj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+/**
+ * PackageName : com.dglee.mini_prj.config
+ * FileName : SecurityConfig
+ * Author : dglee
+ * Create : 2022/12/03 5:24 PM
+ * Description : 스프링 시큐리티는 기본적으로 인증되지 않은 사용자는 서비스를 사용할 수 없게끔 되어 있다.
+ * 따라서 인증을 위한 로그인 화면이 나타나는 것이다.
+ * 하지만 이러한 기본 기능은 게시판 등 로그인 없이 볼 수 있는 페이지에 그대로 적용하기에는 곤란하므로
+ * 시큐리티의 설정을 통해 바로 잡아야 한다.
+ **/
+
+/*
+ * @Configuration은 스프링의 환경설정 파일임을 의미하는 애너테이션이다. 여기서는 스프링 시큐리티의 설정을 위해 사용되었다.
+ * @EnableWebSecurity는 모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만드는 애너테이션이다.
+ * NOTE : @EnableWebSecurity 애너테이션을 사용하면 내부적으로 SpringSecurityFilterChain이 동작하여 URL 필터가 적용된다.
+ **/
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /*
+    스프링 시큐리티의 세부 설정은 SecurityFilterChain 빈을 생성하여 설정할 수 있다. 다음 문장은 모든 인증되지 않은 요청을 허락한다는 의미이다. 따라서 로그인을 하지 않더라도 모든 페이지에 접근할 수 있다.
+    * */
+    /*
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().requestMatchers(
+                new AntPathRequestMatcher("/**")).permitAll()
+        ;
+        return http.build();
+    }
+    */
+
+    /*
+    * NOTE : CSRF란?
+    * CSRF(cross site request forgery)는 웹 사이트 취약점 공격을 방지를 위해 사용하는 기술이다.
+    * 스프링 시큐리티가 CSRF 토큰 값을 세션을 통해 발행하고 웹 페이지에서는 폼 전송시에 해당 토큰을 함께 전송하여
+    * 실제 웹 페이지에서 작성된 데이터가 전달되는지를 검증하는 기술이다.
+    *
+    * 다음과 같은 input 엘리먼트가 폼(form) 태그 밑에 자동으로 생성된 것을 확인할 수 있다.
+    * <input type="hidden" name="_csrf" value="0d609fbc-b102-4b3f-aa97-0ab30c8fcfd4"/>
+    * 스프링 시큐리티에 의해 위와 같은 CSRF 토큰이 자동으로 생성된다. 즉, 스프링 시큐리티는 이렇게 발행한 CSRF 토큰의 값이 정확한지 검증하는 과정을 거친다.
+    * (만약 CSRF 값이 없거나 해커가 임의의 CSRF 값을 강제로 만들어 전송하는 악의적인 URL 요청은 스프링 시큐리티에 의해 블록킹 될 것이다.)
+    * 그런데 H2 콘솔은 이와 같은 CSRF 토큰을 발행하는 기능이 없기 때문에 위와 같은 403 오류가 발생하는 것이다.
+    * (H2 콘솔은 스프링과 상관없는 일반 애플리케이션이다.)
+    * 스프링 시큐리티가 CSRF 처리시 H2 콘솔은 예외로 처리할 수 있도록 다음과 같이 설정 파일을 수정하자.
+    * */
+
+    /*
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().requestMatchers(
+                new AntPathRequestMatcher("/**")).permitAll()
+                .and() //and() - http 객체의 설정을 이어서 할 수 있게 하는 메서드이다.
+                .csrf().ignoringRequestMatchers(
+                        new AntPathRequestMatcher("/h2-console/**")) // /h2-console/로 시작하는 URL은 CSRF 검증을 하지 않는다는 설정이다.
+        ;
+        ;
+        return http.build();
+    }
+    */
+
+    /*
+    * frame 구조로 작성된 화면은 깨진다. 스프링 시큐리티는 사이트의 콘텐츠가 다른 사이트에 포함되지 않도록 하기 위해 X-Frame-Options 헤더값을 사용하여 이를 방지한다.
+    * (clickjacking 공격을 막기위해 사용함)
+    * 이 문제를 해결하기 위해 다음과 같이 설정 파일을 수정하자.
+    * */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().requestMatchers(
+                        new AntPathRequestMatcher("/**")).permitAll()
+                .and()
+                .csrf().ignoringRequestMatchers(
+                        new AntPathRequestMatcher("/h2-console/**"))
+                .and()
+                .headers()
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+        ;
+        return http.build();
+    }
+    // 위 처럼 URL 요청시 X-Frame-Options 헤더값을 sameorigin으로 설정하여 오류가 발생하지 않도록 했다.
+    // X-Frame-Options 헤더의 값으로 sameorigin을 설정하면 frame에 포함된 페이지가 페이지를 제공하는 사이트와 동일한 경우에는 계속 사용할 수 있다.
+}
